e4bb9c6941f0f4beecbf78e16a46f15480097cab
omer ram <0meram95@gmail.com>
2021-12-28 12:13:18 +0200

    stock list call

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,84 +1,47 @@
-import React, { useState, useEffect, useContext } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import PetList from "./PetList";
+import StockList from "./StockList";
+import ClipLoader from "react-spinners/ClipLoader";
 import "./home.css";
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		height: "100vh",
-	},
-	media: {
-		height: 140,
-	},
-	card: {
-		maxWidth: 345,
-	},
-	paper: {
-		backgroundColor: theme.palette.background.paper,
-		border: "2px solid #ffffff",
-		boxShadow: theme.shadows[5],
-		padding: theme.spacing(1, 1, 1),
-		borderRadius: "4px",
-		maxWidth: 1000,
-		margin: "auto",
-	},
-	formControl: {
-		minWidth: 650,
-	},
-	submit: {
-		margin: 4,
-	},
-}));
-
 export default function Home() {
-	const classes = useStyles();
-	const [cardList, setCardList] = useState([1, 2, 3, 4, 5]);
-	const [empty, setEmpty] = useState(false);
-
+	const [cardList, setCardList] = useState([]);
 	const [loading, setLoading] = useState(false);
 
-	const API_KEY = "IVvVFsdJNB113m1ycK0tc3fDNQ4ekKkL734rjO91";
-	const region = "US",
-		lang = "en";
-	const symbols = "BAC%2CKC%3DF%2C002210.KS%2CIWM%2CAMECX";
-
-	// useEffect(() => {
-	// 	axios
-	// 		.get(
-	// 			`https://yfapi.net/v6/finance/quote/marketSummary?lang=en&region=US`,
-	// 			{
-	// 				headers: {
-	// 					accept: "application/json",
-	// 					"X-API-KEY": "IVvVFsdJNB113m1ycK0tc3fDNQ4ekKkL734rjO91",
-	// 				},
-	// 			}
-	// 		)
-	// 		.then(function (response) {
-	// 			setCardList(response.data.marketSummaryResponse.result);
-	// 		});
-	// }, []);
+	useEffect(() => {
+		setLoading(true);
+		axios
+			.get(
+				`https://yfapi.net/v6/finance/quote/marketSummary?lang=en&region=US`,
+				{
+					headers: {
+						accept: "application/json",
+						"X-API-KEY": "IVvVFsdJNB113m1ycK0tc3fDNQ4ekKkL734rjO91",
+					},
+				}
+			)
+			.then(function (response) {
+				setCardList(response.data.marketSummaryResponse.result);
+				setLoading(false);
+			});
+	}, []);
 
 	return (
-		<div className="search-con">
+		<div>
+			<h1 className="title">AudiTech Stocks</h1>
+			{loading && <ClipLoader color="white" loading={true} css="" size={160} />}
 			<div className="list-wrap">
 				<div className="list">
-					{cardList.map(
-						(card, i) => (
-							console.log("~ cardList", cardList),
-							(
-								<PetList
-									card={card}
-									exchange={card.exchange}
-									// regularMarketChangePercent={
-									// 	card.regularMarketChangePercent.fmt
-									// }
-									fullExchangeName={card.fullExchangeName}
-									exchangeTimezoneName={card.exchangeTimezoneName}
-								/>
-							)
-						)
-					)}
+					{cardList.map((card, i) => (
+						<StockList
+							card={card}
+							exchange={card.exchange}
+							region={card.region}
+							shortName={card.shortName}
+							fullExchangeName={card.fullExchangeName}
+							exchangeTimezoneName={card.exchangeTimezoneName}
+						/>
+					))}
 				</div>
 			</div>
 		</div>
--- src/components/home/StockList.jsx
+++ src/components/home/StockList.jsx
@@ -9,28 +9,36 @@ import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import "./home.css";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Paper from "@material-ui/core/Paper";
+import ListSubheader from "@material-ui/core/ListSubheader";
 
 const useStyles = makeStyles({
 	root: {
-		maxWidth: 325,
+		hight: 325,
+		width: 220,
 		margin: 10,
-		overflow: "hidden",
+		padding: 0,
+		overflow: "auto",
 	},
 	modal: {
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "center",
 	},
+	paper: {
+		maxHeight: 500,
+		overflow: "auto",
+		width: 625,
+		margin: 30,
+		padding: 30,
+	},
 });
 
-export default function Search(props) {
+export default function StockList(props) {
 	const classes = useStyles();
 	const [open, setOpen] = useState(false);
-	const obj = {
-		hi: "hi",
-		shalom: {},
-		who: "who"
-	}
 
 	const handleOpen = () => {
 		setOpen(true);
@@ -42,10 +50,16 @@ export default function Search(props) {
 
 	return (
 		<div>
-			<Card className={classes.root} key={props.key} onClick={handleOpen}>
+			<Card
+				className={classes.root}
+				key={props.key}
+				onClick={handleOpen}
+			>
 				<h1>{props.exchange}</h1>
-				<h3>time zone: {props.exchangeTimezoneName}</h3>
-				<p>{props.fullExchangeName}</p>
+				<h4>{props.exchangeTimezoneName}</h4>
+				<span>full name: {props.fullExchangeName}</span>
+				<p>region: {props.region}</p>
+				<span>shortName: {props.shortName}</span>
 				<CardActionArea>
 					<div
 						style={{
@@ -55,8 +69,10 @@ export default function Search(props) {
 						}}
 					>
 						<div>
-							<Typography variant="body2" color="textSecondary" component="p">
-								{props.regularMarketChangePercent}
+							<Typography variant="body2" color="secondary" component="p">
+								{props.card.regularMarketChangePercent
+									? props.card.regularMarketChangePercent.fmt
+									: null}
 							</Typography>
 						</div>
 						<Button size="small" variant="outlined" onClick={handleOpen}>
@@ -78,41 +94,47 @@ export default function Search(props) {
 				}}
 			>
 				<Fade in={open}>
-					<Card className={classes.root} key={props.key} onClick={handleClose}>
-						{/* {Object.keys(props.card).map(
-							(item, i, value) => (
-								(value = props.card[item]),
-								console.log("~ value", value),
-								(
-									<li key={i}>
-										<span>
-											{i} {item}: {!item == {} ? "value" : null}
-										</span>
-									</li>
-								)
-							)
-						)} */}
-						{Object.keys(obj).map(
-							(item, i, value) => (
-								(value = obj[item]),
-								console.log("~ value", value),
-								console.log('~ typeof value === "object"', typeof value === "object"),
+					<Paper
+						className={classes.paper}
+						key={props.key}
+						onClick={handleClose}
+					>
+						<h1>{props.exchange}</h1>
+						<ListSubheader style={{ background: "gray" }}>
+							<h1>{props.exchange}</h1>
+						</ListSubheader>
+						<List>
+							{Object.keys(props.card).map(
+								(item, i, value) => (
+									(value = props.card[item]),
 									(
-										<li key={i}>
+										<ListItem key={i}>
 											<span>
-												{/* {i} {item}: {value || "hi"} */}
-												{i} {item}: {typeof value === "object" && null}
+												<b>{item}:</b>{" "}
+												{typeof value === "object"
+													? Object.keys(value).map((insideValue, index) => {
+															return (
+																<li>
+																	<span>
+																		<>{insideValue}:</>
+																		{value[insideValue]}
+																	</span>
+																</li>
+															);
+													  })
+													: value}
 											</span>
-										</li>
+										</ListItem>
 									)
-							)
-						)}
+								)
+							)}
+						</List>
 						<CardActionArea>
 							<Button size="small" variant="outlined" onClick={handleOpen}>
 								Close
 							</Button>
 						</CardActionArea>
-					</Card>
+					</Paper>
 				</Fade>
 			</Modal>
 		</div>
